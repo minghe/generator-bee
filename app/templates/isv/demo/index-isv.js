5134a03cb23bac3692239ac0205d343dc72ac22d
shiba <lorrylockie@gmail.com>
2014-03-05 10:13:00 +0800

    Update index-isv.js

--- app/templates/isv/demo/index-isv.js
+++ app/templates/isv/demo/index-isv.js
@@ -1,6 +1,6 @@
 //组件适配文档： https://github.com/lorrylockie/tpap/wiki/如何进行前端JS组件的适配工作
 //console.log 是原生api，可以直接使用(非ie低版本)
-console.log(1);
-
 //caja 容器内，默认KISSY对象是有的
+
+console.log(1);
 console.log(KISSY.<%= comConfig.comName %>);
